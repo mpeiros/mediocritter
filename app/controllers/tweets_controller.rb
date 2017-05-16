981eac0af78fd2119809f02732feb948c25afef5
Max Peiros <mpeiros@gmail.com>
2017-05-15 22:06:32 -0700

    Add tweets post route and add sinatra-flash to gemfile

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,16 +1,23 @@
 get '/' do
-  response = Tweet.get_tweets('mpeiros')
-
-  JSON.parse(response.body).each do |tweet|
-    tweet_data = Tweet.parse_tweet(tweet)
-    Tweet.create(tweet_data)
-  end
-
   @tweets = Tweet.all
 
   erb :index
 end
 
-post '/' do
-  
+post '/tweets' do
+  Tweet.find_each(&:destroy)
+
+  response = Tweet.get_tweets(params[:username])
+
+  if response.code == '200'
+    JSON.parse(response.body).each do |tweet|
+      tweet_data = Tweet.parse_tweet(tweet)
+      Tweet.create(tweet_data)
+    end
+    
+    redirect '/'
+  else
+    flash[:error] = 'No tweets found for that username.'
+    redirect '/'
+  end
 end
