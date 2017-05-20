9c279e331f0e6edc03e38c6ef02cd999874979ca
Max Peiros <mpeiros@gmail.com>
2017-05-19 21:10:35 -0700

    Implement post sessions route and add sessions new view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,15 @@ get '/sessions/new' do
 end
 
 post '/sessions' do
-  
+  user = User.authenticate(params[:email], params[:password])
+
+  if user
+    session[:user_id] = user.id
+    redirect '/tweets'
+  else
+    @errors = ['Incorrect Login Information']
+    erb :'sessions/new'
+  end
 end
 
 delete '/sessions/:id' do
